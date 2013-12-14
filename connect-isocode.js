6dbfa756abccb8d8094573f5831b735901965de0
Jason Benterou <jason.benterou@gmail.com>
2013-12-13 19:56:23 -0800

    Cowbell added

--- connect-isocode.js
+++ connect-isocode.js
@@ -5,11 +5,16 @@
  *  or browser emulator.
  * Makes Javascript apps accessible to no-js clients.
  *
- * TODO stream output to headless browser
- *
  * TODO auto-insert isocode.js script tag?
  */
 var cproc = require('child_process');
+
+function error(source) {
+  return function(error) {
+    console.log('Connect-Isocode | Error (' +source+ ') ' + error);
+  };
+};
+
 module.exports = function isocode(driver) {
 
   var driver = driver || 'phantomjs';
@@ -19,27 +24,52 @@ module.exports = function isocode(driver) {
    * See http://www.senchalabs.org/connect/compress.html
    */
   return function(req, res, next) {
+    var setHeader = res.setHeader
     var write = res.write;
+    var writeHead = res.writeHead;
+    var writeHeader = res.writeHeader;
     var end = res.end;
-    var filter = cproc.spawn('bin/drivers/' + driver);
+    var filter = cproc.spawn('isocode', [' --driver=' + driver]);
     var buffer = [];
 
-    filter.stdout.on('data', function (data) {
+    filter.on('error', error('child process'));
+    filter.stderr.on('error', error('stderr'));
+    filter.stdout.on('error', error('stdout'));
+    filter.stdin.on('error', error('stdin'));
+
+    filter.stderr.on('data', function (data) {
+      console.log('STDERR> ', '' + data);
+    });
+
+    filter.stdout.on('data', function (chunk) {
+      console.log('out', arguments);
       buffer.push(chunk);
     });
 
     filter.on('close', function(code) {
+      console.log('close', arguments);
       if (code) {
-        res.statusCode = 500;
+        // TODO maybe a race condition: user app may set statusCode
+        // res.statusCode = 500;
         console.error('Isocode sent status code 500. Headless browser absent or unable to process page.');
-        end.call(res, '{ error: "success" }', encoding);
+        return end.call(res, '{ error: "success" }');
       }
-      end.call(res, buffer.join(''), encoding);
+      console.log(buffer);
+      console.log(buffer.join(''));
+      end.call(res, buffer.join(''));
     });
 
-    res.write = filter.stdin.write;
+    // res.write = filter.stdin.write;
+    res.write = function() {
+      console.log('write', arguments);
+      filter.stdin.write.apply(filter.stdin, arguments);
+    };
 
-    res.end = filter.stdin.end;
+    // res.end = filter.stdin.end;
+    res.end = function() {
+      console.log('end', arguments);
+      filter.stdin.end.apply(filter.stdin, arguments);
+    };
 
     next();
   };
--- test/1/app-simple.js
+++ test/1/app-simple.js
@@ -4,7 +4,7 @@ var express = require('express');
 
 var app = express();
 
-app.use(isocode('phantom'));
+// app.use(isocode('phantom'));
 
 app.get(/.*/, function(req, res) {
 
