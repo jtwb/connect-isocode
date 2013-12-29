cfb6ed9532286a7ddec388585f267eed3ef7f1e8
Jason Benterou <jason.benterou@gmail.com>
2013-12-29 14:06:41 -0800

    Redispatch request via phantomJS and forward response to user

--- connect-isocode.js
+++ connect-isocode.js
@@ -1,11 +1,32 @@
 /*
  * connect-isocode
  *
- * Filter the response by evaluating the page in a headless browser
- *  or browser emulator.
+ * Evaluate the page in a headless browser or browser emulator and respond with the result.
  * Makes Javascript apps accessible to no-js clients.
  *
  * TODO auto-insert isocode.js script tag?
+ *
+ * * On incoming request,
+ *     Send request to isocode
+ *       via command-line args e.g. --header="Host: example.com" --header="User-Agent: a...."
+ *       including the callback port (localhost:8080 etc)
+ *     Respond with result
+ *
+ * * Isocode will replay the user's request
+ *     with the X-Isocode: Bypass header
+ *
+ * * Skip Isocode processing when
+ *   1
+ *   X-Isocode: Bypass header
+ *
+ *   2
+ *   req.url matches options.exclude regex list (e.g. /^api/)
+ *
+ *   3
+ *   User-Agent does not match "SEO" agents iff options.restrict == 'SEO'
+ *
+ *   4
+ *   User-Agent does not match "NoJS" agents iff options.restrict == 'NoJS' (superset of SEO)
  */
 var cproc = require('child_process');
 
@@ -18,17 +39,112 @@ function error(source) {
   };
 };
 
+// TODO support restrict == seo, restrict = nojs
+function should_bypass(req, flags) {
+  var param = req.query['bypass-isocode'];
+  var header = req.headers['x-isocode'];
+  return param || (header && header.toLowerCase() == 'bypass');
+};
 
 
-module.exports = function isocode(driver) {
+module.exports = function isocode(driver, options) {
 
-  var driver = driver || 'phantomjs';
+  if (arguments.length == 1) {
+    options = driver;
+    driver = undefined;
+  }
 
-  /*
-   * Wrap res.end, res.write to filter the output stream.
-   * See http://www.senchalabs.org/connect/compress.html
-   */
   return function(req, res, next) {
+    console.log('connect-isocode:req', req.url);
+
+    console.log('connect-isocode:bypass? ' + (should_bypass(req, options) ? 't' : 'f'));
+    console.log('connect-isocode:headers ', req.headers);
+
+    if (should_bypass(req, options)) { return next(); }
+
+    var isocodeArgs = [];
+    var responseBody = [];
+    var responseHeaders = [];
+
+    if (driver) {
+      isocodeArgs.push('--driver='+driver);
+    }
+
+    // TODO insert the following arguments
+    // * Request method (GET, POST, etc)
+    // * Request path
+    // * Request headers
+    isocodeArgs = isocodeArgs.concat([
+      '--',
+      (req.connection.encrypted ? 'https' : 'http') +
+        '://localhost:' +
+        req.connection.server.address().port +
+        req.url
+    ]);
+
+    var isocode = cproc.spawn('isocode', isocodeArgs);
+
+    isocode.on(        'error', error('child process'));
+    isocode.stderr.on( 'error', error('stderr'));
+    isocode.stdout.on( 'error', error('stdout'));
+    isocode.stdin.on(  'error', error('stdin'));
+
+
+    // TODO recieve metadata from isocode:
+    // * statusCode
+    // * response headers
+    //   preserve order!
+    isocode.stderr.on('data', function (data) {
+      console.error('Connect-Isocode | STDERR> ', '' + data);
+    });
+
+
+
+    // TODO support both Chunked Transfer Encoding and fixed length content modes
+    // using chunked for simplicity
+    // http://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html
+    // just respect the user app's behavior
+    res.setHeader('Transfer-Encoding', 'chunked');
+
+    isocode.stdout.on('data', function (chunk) {
+      DEBUG && console.log('Connect-Isocode | STDOUT>', chunk.toString());
+      res.write.apply(res, arguments);
+    });
+
+
+
+    isocode.on('close', function(code) {
+      DEBUG && console.log('isocode:close', arguments);
+
+      if (code) {
+        res.statusCode = 500;
+        console.error('Isocode sent status code 500. Headless browser absent or unable to process page.');
+        return res.end('{ error: "success" }');
+      }
+
+      // TODO read from real response
+      res.statusCode = 200;
+      // res.setHeader('Content-Length', content.length);
+      res.end();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
     var nativeMethods = {
       setHeader:    res.setHeader,
       write:        res.write,
@@ -139,5 +255,6 @@ module.exports = function isocode(driver) {
     };
 
     next();
+    */
   };
 };
--- test/cats/app.js
+++ test/cats/app.js
@@ -1,16 +1,16 @@
-#!/usr/local/bin/node
+#!/usr/bin/node
 var express = require('express');
  var isocode = require('../../connect-isocode');
 
 var app = express();
 
-app.use(isocode('phantom'));
-
 app.use('/assets', express.static(__dirname + '/assets', { maxAge: 5 }));
+
+app.use(isocode('phantom'));
 
 app.get(/^index|^\//, function(req, res) {
 
-    res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/index-static.html');
 });
 
-app.listen(3141);
+app.listen(3020);
